e7a21a68af9641845c2853fa1033282cdaf97168
DelwinPrakash <delwinprakash499@gmail.com>
2024-09-09 21:03:10 +0530

    fixed bugs in sidebar

--- src/components/sections/ HeroSection.jsx
+++ src/components/sections/ HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <>
-      <header className="bg-gray-900 h-16 flex items-center justify-center">
+      <header className="border-b border-b-white h-16 flex items-center justify-center">
         <div className="h-12 flex items-center justify-between w-screen mx-6">
           <div className="flex items-center h-full">
             <Logo/>
@@ -36,7 +36,7 @@ const HeroSection = () => {
           </button>
         </div>
       </header>
-      <div className={`md:hidden fixed h-full w-full text-white p-4 bg-gray-900 z-50 transition-transform duration-500 ${isOpen? 'translate-x-0' : 'translate-x-full'}`}>
+      <div className={`md:hidden fixed h-full w-full text-white p-4 backdrop-blur-xl z-50 transition-transform duration-500 ${isOpen? 'translate-x-0' : 'translate-x-full'}`}>
         <nav className="mx-1">
           <a href="#services" className="block rounded-lg py-3 px-2 hover:bg-gray-700">Services</a>
           <a href="#pricing" className="block rounded-lg py-3 px-2 hover:bg-gray-700">Pricing</a>
@@ -44,6 +44,15 @@ const HeroSection = () => {
           <a href="#client" className="block rounded-lg py-3 px-2 hover:bg-gray-700">Client</a>
           <a href="#team" className="block rounded-lg py-3 px-2 hover:bg-gray-700">Team</a>
         </nav>
+      </div>
+      <div className="mt-10 w-screen h-80 flex justify-center">
+        <div className="w-4/5 flex flex-col items-center justify-center gap-4">
+          <div className="w-full flex justify-center">
+            <h1 className="text-center text-3xl md:text-5xl flex jusify-center items-center font-bold">Just One Platform,<br/>More Digital Options</h1>
+          </div>
+          <p className="hidden md:block">Navigating the digital landscape for success by Creating solutions for creative designings</p>
+          <button className="bg-[rgba(71,28,195,1)] w-36 h-12 mt-3 rounded-md font-semibold">Get on the call</button>
+        </div>
       </div>
     </>
   );
